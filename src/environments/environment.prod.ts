784f9739c303d2c1fa7e11f14f4241dee4a897c8
apoorv1997 <apoorv.b15@iiits.in>
2018-10-12 15:36:46 +0530

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  config : {
+    apiKey: "AIzaSyDxwrostjK90zQk3opBC3Okn9GmiR3fxwE",
+    authDomain: "pokedex-3e060.firebaseapp.com",
+    databaseURL: "https://pokedex-3e060.firebaseio.com",
+    projectId: "pokedex-3e060",
+    storageBucket: "pokedex-3e060.appspot.com",
+    messagingSenderId: "426735920965"
+  }
 };
